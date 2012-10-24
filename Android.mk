3264ef9f3e9be5ca4cc5b1ea5546b7380de096c5
Chiao Cheng <chiaocheng@google.com>
2012-10-23 18:30:08 -0700

    Fix build breakage.
    Contacts common build file is borked. Taking out of rotation until proper
    fix can be created.
    
    Change-Id: I8598f9ccd8928b10e2dd3be58cc37e0c2d45cd3e

--- Android.mk
+++ Android.mk
@@ -33,4 +33,4 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#include $(call all-makefiles-under,$(LOCAL_PATH))
