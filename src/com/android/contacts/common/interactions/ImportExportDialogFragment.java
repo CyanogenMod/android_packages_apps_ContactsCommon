a8633ff167a9865f91c03683104ed4da59b482f4
Zyg0te <edvard.holst@gmail.com>
2014-09-23 10:02:36 +0200

    Added a WANT_CONTACTS intent extra to indicate that we want to export...
    ...contacts to storage. This intent extra is then used in the Contacts app to ignore the MAX_CONTACTS_NUM_TO_SELECT_ONCE value so that we can export any given number of contacts to storage.
    
    Change-Id: Id3f39b30c545acde1014a4d2a860d5ef022a1b28

--- src/com/android/contacts/common/interactions/ImportExportDialogFragment.java
+++ src/com/android/contacts/common/interactions/ImportExportDialogFragment.java
@@ -120,6 +120,9 @@ public class ImportExportDialogFragment extends DialogFragment
     //decide whether pick phone or contacts
     private static final String IS_CONTACT = "is_contact";
 
+    // indicate that we want to export contacts
+    private static final String WANT_EXPORT = "want_export";
+
     // multi-pick contacts which contains email address
     private static final String ACTION_MULTI_PICK_EMAIL =
         "com.android.contacts.action.MULTI_PICK_EMAIL";
@@ -253,6 +256,7 @@ public class ImportExportDialogFragment extends DialogFragment
                         Intent exportIntent = new Intent(ACTION_MULTI_PICK,
                                 Contacts.CONTENT_URI);
                         exportIntent.putExtra(IS_CONTACT,true);
+                        exportIntent.putExtra(WANT_EXPORT,true);
                         getActivity().startActivityForResult(exportIntent,
                                 SUBACTIVITY_EXPORT_CONTACTS);
                         break;
