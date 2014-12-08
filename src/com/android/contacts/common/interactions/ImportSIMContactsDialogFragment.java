735ed91ec5c35024969c57153c8bba69315ae291
Raj Yengisetty <rajesh@cyngn.com>
2014-12-08 12:35:46 -0800

    Import feature - auto prompt for importing SIM contacts
    Change-Id: I1c24094c3d588edf2d1182d8369882523e6f6512

---
+++ src/com/android/contacts/common/interactions/ImportSIMContactsDialogFragment.java
@@ -0,0 +1,36 @@
+package com.android.contacts.common.interactions;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import com.android.contacts.common.R;
+
+/**
+ * An dialog invoked to import/export contacts.
+ */
+public class ImportSIMContactsDialogFragment extends DialogFragment {
+    public static final String TAG = "ImportSIMContactsDialogFragment";
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.import_contacts_sim)
+                .setPositiveButton(R.string.import_contacts_sim_confirm,
+                        new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        ImportExportDialogFragment.show(getFragmentManager(),
+                                false, ImportSIMContactsDialogFragment.class);
+                    }})
+                .setNegativeButton(R.string.import_contacts_sim_cancel,
+                        new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // Nothing to do
+                        dismiss();
+                    }});
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+}
