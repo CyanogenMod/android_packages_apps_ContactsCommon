c0f59f699d412ff20137da9458c25d649129999b
Rohit Yengisetty <rohit@cyngn.com>
2015-04-13 18:19:21 -0700

    ContactsCommon : Modify ContactPhotoManager to fetch contacts' bitmaps
    Change-Id: If7279a5a77c9d036c5ffdf0633c7577df30973b4
    (cherry picked from commit ad0dff5a4331a1625c51c8b9cbcdbfbe5364dae2)

--- TestCommon/src/com/android/contacts/common/test/mocks/MockContactPhotoManager.java
+++ TestCommon/src/com/android/contacts/common/test/mocks/MockContactPhotoManager.java
@@ -46,6 +46,11 @@ public class MockContactPhotoManager extends ContactPhotoManager {
     }
 
     @Override
+    public void getBitmapForContact(Uri photoUri, ImageView imgView, int widthHint,
+            PhotoFetcherCallback cb) {
+    }
+
+    @Override
     public void removePhoto(ImageView view) {
         view.setImageDrawable(null);
     }
