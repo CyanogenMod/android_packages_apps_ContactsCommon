371f4f7164f258f996b1ee6b4a93f6e38a7128df
Jorge Ruesga <jorge@ruesga.com>
2015-06-01 21:59:51 +0200

    contacts-commongs: remove unneded imports
    Change-Id: I9207b38282ad5d9cf8934aff89d73a9fe8e34254
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/contacts/common/util/PhoneNumberHelper.java
+++ src/com/android/contacts/common/util/PhoneNumberHelper.java
@@ -23,9 +23,6 @@ import android.util.Log;
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
-import com.google.i18n.phonenumbers.ShortNumberInfo;
-
-import java.util.Locale;
 
 /**
  * This class wraps several PhoneNumberUtil calls and TelephonyManager calls. Some of them are
