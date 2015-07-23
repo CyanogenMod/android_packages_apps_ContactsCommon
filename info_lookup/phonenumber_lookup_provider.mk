00325be4df8b4a77c1a47dee2445609900d66eb5
Rohit Yengisetty <rohit@cyngn.com>
2015-07-23 10:40:30 -0700

    PhoneNumberLookup : have the mk file hold its absolute path for easy inclusion
    Change-Id: I0e51ae309a929faa41f3f397556c51090998c576

--- info_lookup/phonenumber_lookup_provider.mk
+++ info_lookup/phonenumber_lookup_provider.mk
@@ -1,1 +1,2 @@
-LOCAL_SRC_FILES += $(call all-java-files-under, info_lookup/src/com/cyanogen/lookup/phonenumber)
+CONTACTS_COMMON_PHONE_NUMBER_LOOKUP := ../../../packages/apps/ContactsCommon/info_lookup/src/com/cyanogen/lookup/phonenumber
+LOCAL_SRC_FILES += $(call all-java-files-under, $(CONTACTS_COMMON_PHONE_NUMBER_LOOKUP))
