fa369cd8ca1a7aebfb2cc2f8a4206b32906ce1f2
Martin Brabham <optedoblivion@cyngn.com>
2015-10-05 08:08:20 -0700

    Shim in mark as spam api
    Change-Id: I77cd6741befa949a6938a0ba118a0f196a0045b3
    Ticket-Id: MMS-158

--- info_lookup/src/com/cyanogen/lookup/phonenumber/LookupHandlerThread.java
+++ info_lookup/src/com/cyanogen/lookup/phonenumber/LookupHandlerThread.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
 import com.cyanogen.lookup.phonenumber.provider.LookupProviderImpl;
 import com.cyanogen.lookup.phonenumber.request.LookupRequest;
 
@@ -12,6 +14,8 @@ import java.util.HashSet;
 public class LookupHandlerThread extends HandlerThread implements Handler.Callback {
 
     private static final int MSG_FETCH_INFO = 0;
+    private static final int MSG_MARK_AS_SPAM = 1;
+    private static final String TAG = LookupHandlerThread.class.getSimpleName();
 
     private Context mContext;
     private Handler mHandler;
@@ -41,6 +45,8 @@ public class LookupHandlerThread extends HandlerThread implements Handler.Callba
             mSubmittedRequests = new HashSet<>();
             start();
             mHandler = new Handler(getLooper(), this);
+        } else {
+            Log.w(TAG, "Failed to initialize!");
         }
 
         return mInitialized;
@@ -72,15 +78,39 @@ public class LookupHandlerThread extends HandlerThread implements Handler.Callba
         return false;
     }
 
+    /**
+     * Posts a message to {@link #mHandler} which later dispatches a request to the provider
+     * implementation that knows how to mark a phone number as spam
+     *
+     * @param phoneNumber {@link String}
+     */
+    public void markAsSpam(String phoneNumber) {
+        if (TextUtils.isEmpty(phoneNumber)) {
+            return;
+        }
+        if (mHandler != null) {
+            Message msg = mHandler.obtainMessage(MSG_MARK_AS_SPAM);
+            msg.obj = phoneNumber;
+            mHandler.sendMessage(msg);
+        } else {
+            Log.w(TAG, "No handler!");
+        }
+    }
+
     @Override
     public boolean handleMessage(Message msg) {
         int what = msg.what;
-        LookupRequest lookupRequest = (LookupRequest) msg.obj;
         switch (what) {
             case MSG_FETCH_INFO :
                 if (mInitialized) {
-                    mLookupProvider.fetchInfo(lookupRequest);
+                    mLookupProvider.fetchInfo((LookupRequest) msg.obj);
                 }
+                break;
+            case MSG_MARK_AS_SPAM :
+                if (mInitialized) {
+                    mLookupProvider.markAsSpam((String) msg.obj);
+                }
+                break;
         }
         return true;
     }
