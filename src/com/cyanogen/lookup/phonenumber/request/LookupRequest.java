8d3697c95e8ad5b224b754037adf64c494d1fd6c
Rohit Yengisetty <rohit@cyngn.com>
2016-03-16 12:00:41 -0700

    Add RequestOrigin to LookupRequest
    This new field will help identify the origin and reason behind
    the LookupRequest.
    
    Change-Id: Iaf547a50c8113abac3ca9339cc289cda4357d718

--- src/com/cyanogen/lookup/phonenumber/request/LookupRequest.java
+++ src/com/cyanogen/lookup/phonenumber/request/LookupRequest.java
@@ -6,12 +6,37 @@ import com.cyanogen.lookup.phonenumber.response.LookupResponse;
  * Encapsulates the notion of a phone number lookup request
  */
 public class LookupRequest {
+    /**
+     * Used to identify the reason behind the request
+     */
+    public enum RequestOrigin {
+        UNSPECIFIED,
+        INCOMING_CALL,
+        OUTGOING_CALL,
+        INCOMING_SMS,
+        OUTGOING_SMS,
+        OTHER
+    }
+
+    /**
+     * phone number must be in an E164 format
+     */
     public String mPhoneNumber;
     public Callback mCallback;
+    public RequestOrigin mRequestOrigin;
 
     public LookupRequest(String phoneNumber, Callback callback) {
+        this(phoneNumber, callback, RequestOrigin.UNSPECIFIED);
+    }
+
+    public LookupRequest(String phoneNumber, Callback callback, RequestOrigin origin) {
         mPhoneNumber = phoneNumber;
         mCallback = callback;
+        mRequestOrigin = origin;
+    }
+
+    public void setRequestOrigin(RequestOrigin type) {
+        mRequestOrigin = type;
     }
 
     @Override
