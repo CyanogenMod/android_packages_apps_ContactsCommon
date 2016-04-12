d4b8508e7279b1a7aab72e2d62fa68c13a0b602d
Stephen Bird <sbird@cyngn.com>
2016-04-12 10:42:09 -0700

    Remove duplicate ambientsdk reference
    Change-Id: I92851e7dec1aa5ee4a922150af67326c9a2223c7

--- Android.mk
+++ Android.mk
@@ -30,9 +30,6 @@ LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages com.android.phone.common
 
-LOCAL_STATIC_JAVA_AAR_LIBRARIES += \
-	ambientsdk
-
 LOCAL_STATIC_JAVA_LIBRARIES := \
     com.android.vcard \
     guava \
