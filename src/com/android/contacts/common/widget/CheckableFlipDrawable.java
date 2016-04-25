1c3d9f241d37742e3e507853e409a13c39c7ac4a
Stephen Bird <sbird@cyngn.com>
2016-04-25 16:42:50 -0700

    Revert "Remove scaling option for the Front drawable in CheckableFlipDrawable"
    This reverts commit 9590c56a1ff722255bfb1d701b3eb38e52b8a70c.
    
    Change-Id: I2523cf9a0c0a446ec83dd34ca27c1b2cdc1b2e52

--- src/com/android/contacts/common/widget/CheckableFlipDrawable.java
+++ src/com/android/contacts/common/widget/CheckableFlipDrawable.java
@@ -70,6 +70,10 @@ public class CheckableFlipDrawable extends FlipDrawable implements
         invalidateSelf();
     }
 
+    public void scaleFrontDrawableToBounds(boolean fitBounds) {
+        mFrontDrawable.scaleDrawableToBounds(fitBounds);
+    }
+
     public void setCheckMarkBackgroundColor(int color) {
         mCheckmarkDrawable.setBackgroundColor(color);
         invalidateSelf();
@@ -125,6 +129,8 @@ public class CheckableFlipDrawable extends FlipDrawable implements
 
     private static class FrontDrawable extends Drawable implements Drawable.Callback {
         private Drawable mDrawable;
+        private boolean mScaleDrawableToBounds = false; // only applies to drawables with intrinsic
+                                                        // height and width
 
         public FrontDrawable(Drawable d) {
             mDrawable = d;
@@ -141,6 +147,10 @@ public class CheckableFlipDrawable extends FlipDrawable implements
             mDrawable.setCallback(this);
             assignDrawableBounds(getBounds());
             invalidateSelf();
+        }
+
+        public void scaleDrawableToBounds(boolean fitBounds) {
+            mScaleDrawableToBounds = fitBounds;
         }
 
         @Override
@@ -187,12 +197,16 @@ public class CheckableFlipDrawable extends FlipDrawable implements
             if (w <= 0 || h <= 0) {
                 mDrawable.draw(canvas);
             } else {
-                final float widthScale = (float) bounds.width() / (float) w;
-                final float heightScale = (float) bounds.height() / (float) h;
-                final float scale = Math.max(widthScale, heightScale);
                 canvas.save();
-                canvas.scale(scale, scale);
-                canvas.translate(bounds.left, bounds.top);
+                if (mScaleDrawableToBounds) {
+                    final float widthScale = (float) bounds.width() / (float) w;
+                    final float heightScale = (float) bounds.height() / (float) h;
+                    final float scale = Math.max(widthScale, heightScale);
+                    canvas.scale(scale, scale);
+                    canvas.translate(bounds.left, bounds.top);
+                } else {
+                    canvas.translate(bounds.centerX() - (w/2f), bounds.centerY() - (h/2f));
+                }
                 mDrawable.draw(canvas);
                 canvas.restore();
             }
